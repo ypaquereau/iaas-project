9d4ed119db50f540f9146eb0559794b2c2281ba5
Yoann Paquereau <yoann.paquereau@gmail.com>
2021-01-21 09:40:57 +0100

    add server directory

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function () {
+  console.log("Server is running on Port:", PORT);
+});
